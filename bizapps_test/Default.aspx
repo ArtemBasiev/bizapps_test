445008f54930a133d2d1543313fac338e97eeb17
ArtemBasiev <artembasiev@mail.ru>
2018-12-09 17:37:58 +0300

    ended commit

---
+++ bizapps_test/Default.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="bizapps_test.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Добавление категории</title>
+    <link rel="stylesheet" href="StyleTest.css"/>
+</head>
+<body>
+   <form id="form1" runat="server">
+     <div id ="headdiv">
+            <asp:Label ID="headlabel" runat="server" Text="Модуль создания категорий"></asp:Label>
+     </div>
+     <div aria-orientation="horizontal" id="div_union" style="display: inline-block; position: absolute; left: auto; float: none;">
+   
+            <div aria-orientation="horizontal" style="display: block; position: relative; left: 0px; float: left; top: 0px; text-align: left; padding-left: 20px; padding-top: 20px;" id="div_list_kat">
+                <asp:Label ID="Label1" runat="server" Text="Список категорий"></asp:Label><br/>
+                 <asp:BulletedList ID="KategoryList" runat="server"></asp:BulletedList>
+            </div>
+            <div aria-orientation="horizontal" style="display: block; position: static;  right: 50%; text-align: center; overflow: hidden; vertical-align: top; padding-top: 20px;" id ="div_cr_kat">
+                 <asp:Label ID="Label2" runat="server" Text="Создание категории"></asp:Label> <br/><br/><br/>
+                 <asp:Label ID="Label3" runat="server" Text="Введите название категории"></asp:Label><br/><br/>
+                 <asp:TextBox id="Text1"  runat="server"/><br/>
+                 <asp:Label ID="LabelMes" runat="server" Font-Size="12px" ></asp:Label><br/><br/>
+                 <asp:Button id="cr_kat" Text="Создать" runat="server" OnClick="cr_kat_Click" /><br/>
+            </div>
+          
+        
+     </div>
+   </form>
+</body>
+</html>
---
+++ bizapps_test/Default.aspx.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using FirebirdSql.Data.FirebirdClient;
+using System.Data;
+using System.Drawing;
+
+namespace bizapps_test
+{
+    public partial class Default : System.Web.UI.Page
+    {
+
+        public FbConnection fb;
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //////////////////////////////////Выполняем подключение к базе данных
+            try
+            {
+                string path = Server.MapPath(@"DataBase\BIZ_TEST.FDB");
+
+                FbConnectionStringBuilder fb_con = new FbConnectionStringBuilder();
+
+                fb_con.Charset = "UTF8";
+                fb_con.UserID = "SYSDBA";
+                fb_con.Password = "masterkey";
+                fb_con.Database = path;
+                fb_con.ServerType = 0;
+                fb = new FbConnection(fb_con.ToString());
+                fb.Open();
+
+                ////////////////////////////////Заполняем список категорий
+                FbCommand sqlforkat = new FbCommand("select KAT from SEL_KATEGORY", fb);
+                FbDataReader readerkat = sqlforkat.ExecuteReader();
+                DataTable dt_kat = new DataTable();
+                dt_kat.Load(readerkat);
+
+                KategoryList.Items.Clear();
+
+                 for (int i=0; i<dt_kat.Rows.Count; i++)
+                  {
+                      
+                KategoryList.Items.Add(dt_kat.Rows[i][0].ToString());
+                 }
+                
+
+            }
+            catch
+            {
+                LabelMes.ForeColor = Color.Red;
+                LabelMes.Text = "Проблемы с подключением к БД";
+            }
+        }
+
+        protected void cr_kat_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ////////////////////////////////Выполняем проверку создаваемой категории
+                FbCommand sqlforname = new FbCommand("select * from CREATE_KATEGORY('" + Text1.Text.ToUpper()+ "')", fb);
+                FbDataReader readername = sqlforname.ExecuteReader();
+                DataTable dt_name = new DataTable();
+                dt_name.Load(readername);
+
+                if (dt_name.Rows[0][1].ToString() == "1")
+                {
+                    LabelMes.ForeColor = Color.Green;
+                }
+                else
+                {
+                    LabelMes.ForeColor = Color.Red;
+                }
+                LabelMes.Text = dt_name.Rows[0][0].ToString();
+            }
+            catch
+            {
+                LabelMes.ForeColor = Color.Red;
+                LabelMes.Text = "Проблемы с подключением к БД";
+            }
+            Text1.Text = "";
+        }
+    }
+}
